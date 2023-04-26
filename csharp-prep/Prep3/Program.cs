323c6a2d4546a446f96d63aa03da078bf4febdba
Bridger Allen <all22006@byui.edu>
2023-04-26 11:13:18 -0600

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess_count = 0;
+        int guess = 0;
+        Random rnd = new Random();
+        int random_int = rnd.Next(1,20);
+
+        Console.WriteLine($"What is the magic number? {random_int}");
+
+        while (guess != random_int)
+        {
+            Console.Write("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+            if (guess > random_int)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < random_int)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            guess_count += 1;
+        }
+        
+        Console.WriteLine($"You guessed it in {guess_count} guesses!");
     }
 }
