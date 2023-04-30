92142a1cbce3c1a96f7ae66d87fd39cbcfde354c
Bridger Allen <all22006@byui.edu>
2023-04-29 20:07:03 -0600

    Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        float UserNumber = PromptUserNumber();
+        float SquaredNumber = SquareNumber(UserNumber);
+
+        DisplayResult(userName, SquaredNumber);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string UserName = Console.ReadLine();
+        return UserName; 
+    }
+
+    static float PromptUserNumber()
+    {
+        Console.Write("Please enter your number: ");
+        string UserNumber = Console.ReadLine();
+        float UserFloat = float.Parse(UserNumber);
+        return UserFloat; 
+    }
+
+    static float SquareNumber(float ToBeSquared)
+    {
+        float SquaredValue = ToBeSquared * ToBeSquared;
+        return SquaredValue;
+    }
+
+    static void DisplayResult(string UserName, float SquaredValue)
+    {
+        Console.WriteLine($"{UserName}, the square of your number is {SquaredValue}");
+    }
+
 }
