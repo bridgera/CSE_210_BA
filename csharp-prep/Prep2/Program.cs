fa6dc8f7accc20cca299bc59a52487d45644859f
Bridger Allen <all22006@byui.edu>
2023-04-29 20:48:54 -0600

    grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+        string grade_letter = "";
+        int modifier_value = grade % 10;
+        string modifier_sign = "";
+
+        if(grade > 90)
+        {
+            grade_letter = "A";
+        }
+
+        else if(grade > 80)
+        {
+            grade_letter = "B";
+        }
+
+        else if(grade > 70)
+        {
+            grade_letter = "C";
+        }
+
+        else if(grade > 60)
+        {
+            grade_letter = "D";
+        }
+
+        else if(grade < 60)
+        {
+            grade_letter = "F";
+        }
+
+
+        //Set Modifier Sign
+        if(modifier_value >= 7)
+        {
+            modifier_sign = "+";
+        } 
+
+        else if(modifier_value < 3)
+        {
+            modifier_sign = "-";
+        }
+
+
+
+        Console.WriteLine($"Your letter grade is: {grade_letter + modifier_sign}");
     }
 }
